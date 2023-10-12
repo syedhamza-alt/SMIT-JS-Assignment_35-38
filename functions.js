1a9c79e33562402080cc356d993123be13dddcbd
syedhamza-alt <syedhamzaimran3@gmail.com>
2023-10-12 22:12:02 +0500

    Commit Completed

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@
 // Chapters 35 - 38
 
 //Q1
+document.write("1) ");
 function dateFunction() {
   const date = Date();
   return document.write(date + "<br><br>");
@@ -9,12 +10,14 @@ function dateFunction() {
 dateFunction();
 
 //Q2
+document.write("2) ");
 function greetFunction(firstName, lastName) {
   return document.write(`Hello ${firstName + lastName}.<br><br>`);
 }
 greetFunction("Hamza", " Imran");
 
 //Q3
+document.write("3) ");
 function sumFunction() {
   let num1 = Number(prompt("Enter first number", 5));
   let num2 = Number(prompt("Enter second number", 10));
@@ -24,6 +27,7 @@ function sumFunction() {
 sumFunction();
 
 //Q4
+document.write("4) ");
 function desiredOperation(num1, num2, operation) {
   switch (operation) {
     case "+":
@@ -45,16 +49,22 @@ function desiredOperation(num1, num2, operation) {
   }
 }
 document.write(
-  ` The answer of desired operation is :  ${desiredOperation(10, 2, "*")}<br><br>`
+  ` The answer of desired operation is :  ${desiredOperation(
+    10,
+    2,
+    "*"
+  )}<br><br>`
 );
 
 //Q5
+document.write("5) ");
 function squareFunction(squareNumber) {
-    return squareNumber * squareNumber;
+  return squareNumber * squareNumber;
 }
 document.write(`The square of the number is : ${squareFunction(5)}<br><br>`);
 
 //Q6
+document.write("6) ");
 function factorialFunction(factorialNumber) {
   for (let i = factorialNumber - 1; i > 0; i--) {
     factorialNumber = factorialNumber * i;
@@ -64,6 +74,7 @@ function factorialFunction(factorialNumber) {
 document.write(`The factorial is : ${factorialFunction(8)}<br><br>`);
 
 //Q7
+document.write("7) ");
 function counting(startNum, endNum) {
   document.write("Counting is : ");
   for (let i = startNum; i <= endNum; i++) {
@@ -81,6 +92,7 @@ counting(
 );
 
 //Q8
+document.write("8) ");
 function hypotenusFunction(base, perpendicular) {
   var hypotenuse = base * perpendicular;
 
@@ -98,6 +110,7 @@ hypotenusFunction(
 );
 
 //Q9
+document.write("9) ");
 function areaOfRectengle(width, height) {
   let area = width * height;
   return area;
@@ -116,30 +129,34 @@ document.write(
 );
 
 //Q10
-const string_10="madam";
+document.write("10) ");
+const string_10 = "madam";
 function checkIfPalindrome(str) {
-  str=str.toString().split("").reverse().join("");
-  if(string_10 == str){
+  str = str.toString().split("").reverse().join("");
+  if (string_10 == str) {
     document.write(`The string ${string_10} is a palindrome string <br><br>`);
-  }else{
-    document.write(`The string ${string_10} is not a palindrome string <br><br>`);
+  } else {
+    document.write(
+      `The string ${string_10} is not a palindrome string <br><br>`
+    );
   }
 }
 checkIfPalindrome(string_10);
 
 //Q11
+document.write("11) ");
 function toTitleCase(str) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
-string_11="the quick brown fox jumps"
-document.write(`After converting into TitleCase : ${toTitleCase(string_11)} <br><br>`);
+string_11 = "the quick brown fox jumps";
+document.write(
+  `After converting into TitleCase : ${toTitleCase(string_11)} <br><br>`
+);
 
 //Q12
+document.write("12) ");
 function find_longest_word(str) {
   var array1 = str.match(/\w[a-z]{0,}/gi);
   var result = array1[0];
@@ -153,10 +170,13 @@ function find_longest_word(str) {
 }
 const string_12 = "Web Development Tuotorial";
 document.write(
-  `The longest word in '${string_12}' is : '${find_longest_word(string_12)}'.<br><br>`
+  `The longest word in '${string_12}' is : '${find_longest_word(
+    string_12
+  )}'.<br><br>`
 );
 
 //Q13
+document.write("13) ");
 function charAt_Fun(string_13, letter) {
   var countLetter = 0;
   for (let i = 0; i < string_13.length; i++) {
@@ -178,14 +198,17 @@ document.write(
 );
 
 //14
+document.write("14) ");
 function calcCircumference(radius) {
-  const circumferenceCirc= (2 * pi * radius).toFixed(2);
-  return document.write( `The circumference of Cirle is : ${circumferenceCirc}<br>`);
+  const circumferenceCirc = (2 * pi * radius).toFixed(2);
+  return document.write(
+    `The circumference of Cirle is : ${circumferenceCirc}<br>`
+  );
 }
 function calcArea(radius) {
-  const areaCircle= (pi * radius * radius).toFixed(2);
+  const areaCircle = (pi * radius * radius).toFixed(2);
   return document.write(`The area of circle is : ${areaCircle}<br><br>`);
 }
-const pi=3.14;
+const pi = 3.14;
 calcCircumference(5);
 calcArea(5);
